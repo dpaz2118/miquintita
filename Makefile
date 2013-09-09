8584c65628f5a23fa0b879707bdf8086affc4205
dpaz2118 <dante.paz@gmail.com>
2013-09-09 19:12:11 -0300

    Falla la integracion

--- Makefile
+++ Makefile
@@ -16,38 +16,22 @@ ifeq ($(SYSTEM),wepwawet)
 IFC = gfortran
 endif
 
-oscila2.x: nrtype.o parametros_oscila.o nrutil_DP.o nr_DP.o \
-           rkck_DP.o rkqs_DP.o odeint_DP.o oscila2.o 
-	$(IFC) $(OPTION) -o oscila2.x nrtype.o parametros_oscila.o nrutil_DP.o nr_DP.o \
-		                      rkck_DP.o rkqs_DP.o odeint_DP.o oscila2.o 
+OBJS =  nrtype.o            \
+	parametros_oscila.o \
+	nrutil_DP.o         \
+	nr_DP.o             \
+        rkck_DP.o           \
+        rkqs_DP.o           \
+	odeint_DP.o
 
-nrtype.o:  nrtype.f90 
-	$(IFC) $(OPTION) -c   nrtype.f90 
+oscila2.x: $(OBJS) oscila2.o 
+	$(IFC) $(OPTION) -o oscila2.x $(OBJS) oscila2.o 
+
+.SUFFIXES: $(SUFFIXES) .f90
 
 
-parametros.o:  parametros.f90 
-	$(IFC) $(OPTION) -c  parametros.f90 
-
-parametros_oscila.o:  parametros_oscila.f90 
-	$(IFC) $(OPTION) -c  parametros_oscila.f90 
-
-nrutil_DP.o:  nrutil_DP.f90 
-	$(IFC) $(OPTION) -c nrutil_DP.f90 
-
-nr_DP.o:  nr_DP.f90 
-	$(IFC) $(OPTION) -c nr_DP.f90 
-
-rkck_DP.o:  rkck_DP.f90 
-	$(IFC) $(OPTION) -c rkck_DP.f90 
-
-rkqs_DP.o:  rkqs_DP.f90 
-	$(IFC) $(OPTION) -c  rkqs_DP.f90 
-
-odeint_DP.o:  odeint_DP.f90 
-	$(IFC) $(OPTION) -c  odeint_DP.f90 
-
-oscila2.o: oscila2.f90
-	$(IFC) $(OPTION) -c oscila2.f90
+.f90.o:
+	$(IFC) $(OPTION) -c $<
 
 clean:
 	rm *.mod *.o *.x
--- quintimodels.R
+++ quintimodels.R
@@ -1,6 +1,7 @@
-
-options(x11updates=0.25)
 library("sfsmisc")
+library("deSolve")
+options(x11updates=0.25)
+
 
 #tabla de parametros
 tmodel <- read.table("params_model.dat",col.names=c("w0","wm","am","dm"),
@@ -13,7 +14,89 @@ rownames(lsty)=c("inv1","inv2","sugra","exp2","as","cnr")
 lsty <- as.data.frame(lsty)
 assign("lsty",lsty, envir = .GlobalEnv)
 
-w.model <-function(model.name,a) {
+#parametro de densidad de materia
+assign("omega.mat",0.3, envir = .GlobalEnv)
+
+
+plot.wall <- function(new.dev) {
+  if(missing(new.dev)){new.dev=FALSE}
+  plot.w("cnr",new.dev)
+  plot.w("inv1")
+  plot.w("inv2")
+  plot.w("sugra")
+  plot.w("exp2")
+  plot.w("as")
+
+}
+
+plot.xall <- function(new.dev) {
+  if(missing(new.dev)){new.dev=FALSE}
+  plot.xfun("cnr",new.dev)
+  plot.xfun("inv1")
+  plot.xfun("inv2")
+  plot.xfun("sugra")
+  plot.xfun("exp2")
+  plot.xfun("as")
+
+}
+
+plot.w <- function(model.name,new.dev) {
+
+ a=seq(-2,0,0.01)
+ a=10.0^a
+
+ if(missing(new.dev)){new.dev=FALSE}
+ if(attr(dev.cur(),"names")== "null device" || new.dev) {
+    dev.new()
+    par(tcl=1)
+    plot(a, w.model(a,model.name), type = "l",
+	 xlab="a",ylab="w(a)",
+	 xaxt = "n",yaxt="n", log = "x",lty=lsty[model.name,])
+    eaxis(1,at=c(1E-2,1E-1,1))
+    eaxis(2,small.mult=4)
+    eaxis(3,labels=FALSE,at=c(1E-2,1E-1,1))
+    eaxis(4,labels=FALSE)
+ } else {
+    lines(a,w.model(a,model.name),lty=lsty[model.name,])
+ }
+	
+}
+
+plot.xfun <- function(model.name,new.dev) {
+
+ a=seq(-2,0,0.01)
+ a=10.0^a
+
+ if(missing(new.dev)){new.dev=FALSE}
+ if(attr(dev.cur(),"names")== "null device" || new.dev) {
+    dev.new()
+    par(tcl=1)
+    plot(a, X.fun(a,model.name), type = "l",
+	 xlab="a",ylab="X(a)",
+	 xaxt = "n",yaxt="n", log = "x",lty=lsty[model.name,])
+    eaxis(1,at=c(1E-2,1E-1,1))
+    eaxis(2,small.mult=4)
+    eaxis(3,labels=FALSE,at=c(1E-2,1E-1,1))
+    eaxis(4,labels=FALSE)
+ } else {
+    lines(a,X.fun(a,model.name),lty=lsty[model.name,])
+ }
+	
+}
+
+X.fun <- function(a,model.name) {
+	res=a*0.0
+	n <-length(a)
+	for(i in 1:n){
+		ff=integrate(w.model,lower=a[i],upper=1,model.name)
+		res[i]=ff$value
+	}
+
+	res=omega.mat/(1-omega.mat)*exp(-3*res)
+	return(res)
+}
+
+w.model <-function(a,model.name) {
 	
    w0<-tmodel[model.name,]$w0
    wm<-tmodel[model.name,]$wm
@@ -31,19 +114,25 @@ w.model <-function(model.name,a) {
    return(w.phi)
 }
 
-plot.w <- function(model.name) {
+evol.grow.G <- function(model.name) {
+	parms <- c(name = model.name)
+        times <- seq(0.01, 1, length = 101)
+	xstart <- c(x1 = 0.001, x2 = 0.001) #condiciones iniciales para el factor de 
+	                            #crecimiento y sus derivadas
+        out <- ode(xstart,times,dgrow.G,parms)
 
- a=seq(-2,0,0.01)
- a=10.0^a
+}
 
- if(attr(dev.cur(),"names")== "null device") {
-    dev.new()
-    par(tcl=1)
-    plot(a, w.model(model.name,a), type = "l", xaxt = "n",yaxt="n", log = "x",lty=lsty[model.name,])
-    eaxis(1,at=c(1E-2,1E-1,1))
-    eaxis(2,small.mult=4)
- } else {
-    lines(a,w.model(model.name,a),lty=lsty[model.name,])
- }
-	
+
+dgrow.G <- function(a,yfunc,parms)
+{
+         with(as.list(c(parms, yfunc)), {
+	      #se queja de que x2 es no numerico o que es character hay algo roto
+         dx1 <- 0.0#x2
+         dx2 <- 0.0#-(7/2-3/2*w.model(a,name)/(1+X.fun(a,name)))*x2/a
+	 dx2 <- 0.0# dx2 -3/2*(1-w.model(a,name))/(1+X.fun(a,name))*x1/a/a
+         res <- c(dx1, dx2)
+         return(list(res))
+       })
+
 }
